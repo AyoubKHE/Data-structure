42c25b961f9a0322622a1cf517ae9f9b6116475f
Ayoub <itsayoubkheyar06@gmail.com>
2024-08-04 19:44:20 +0100

    implementing push methods of myDynamicArray

--- data_structures/myDynamicArray.h
+++ data_structures/myDynamicArray.h
@@ -13,18 +13,79 @@ private:
     int length;
     int capacity;
 
+    /*
+        The variable highestIndex is used to track the highest index that contains a value.
+        For example, consider the following array:
+
+        __________________________________________________________________________
+        |      |       |        |         |         |       |         |          |
+        |  45  |  87   |   __   |   39    |   __    |   24  |   __    |   __     |
+        |      |       |        |         |         |       |         |          |
+        __________________________________________________________________________
+
+        In this array, the highestIndex would be 6 (using one-based indexing).
+    */
+    int highestIndex;
+
+    void grow(int newCapacity)
+    {
+        int *newArray = new int[newCapacity];
+        for (int i = 0; i < this->highestIndex; i++)
+        {
+            newArray[i] = this->array[i];
+        }
+
+        delete this->array;
+        this->array = newArray;
+        this->capacity = newCapacity;
+        newArray = NULL;
+    }
+
 public:
     myDynamicArray(int capacity)
     {
         this->length = 0;
+        this->highestIndex = 0;
         this->capacity = capacity;
 
         this->array = new int[capacity];
 
         for (int i = 0; i < this->capacity; i++)
         {
-            array[i] = INT_MIN;
+            this->array[i] = INT_MIN;
         }
+    }
+
+    void push(int value)
+    {
+        if (this->highestIndex == this->capacity)
+        {
+            this->grow(this->capacity + 1);
+        }
+
+        this->array[this->highestIndex] = value;
+        this->highestIndex++;
+
+        this->length++;
+    }
+
+    void push(vector<int> &arr)
+    {
+        if ((this->capacity - this->highestIndex) < arr.size())
+        {
+            int newCapacity = this->capacity + arr.size() - (this->capacity - this->highestIndex);
+
+            this->grow(newCapacity);
+        }
+
+        int j = 0;
+        for (int i = this->highestIndex; i < this->highestIndex + arr.size(); i++)
+        {
+            this->array[i] = arr[j];
+            j++;
+        }
+        this->length += arr.size();
+        this->highestIndex += arr.size();
     }
 
     void show()
--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 int main()
 {
-    myDynamicArray *dynamicArray = new myDynamicArray(5);
+    myDynamicArray *dynamicArray = new myDynamicArray(10);
+
+    dynamicArray->push(1);
+    dynamicArray->push(2);
+    dynamicArray->push(3);
+
+    vector<int> arr = {4, 5, 6, 7, 8, 9, 10};
+
+    dynamicArray->push(arr);
+
+    vector<int> arr2 = {95, 96, 97, 98, 99, 100};
+
+    dynamicArray->push(arr2);
+
     dynamicArray->show();
 
     return 0;
